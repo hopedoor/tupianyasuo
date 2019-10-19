45f8b02a6887737e59a884691b85dfc8554ff7e1
hopedoor <52458086+hopedoor@users.noreply.github.com>
2019-10-20 02:37:17 +0800

    Add files via upload

---
+++ update.bat
@@ -0,0 +1,10 @@
+del /f /s /q "%cd%\*.exe"
+del /f /s /q "%cd%\update.txt" 
+del /f /s /q "%cd%\new.txt" 
+del /f /s /q "%cd%\update.vbs" 
+del /f /s /q "%cd%\tupianyasuo-master" 
+ren "%cd%\Í¼Æ¬Ñ¹Ëõ×ª»».exe.bak" Í¼Æ¬Ñ¹Ëõ×ª»».exe
+start Í¼Æ¬Ñ¹Ëõ×ª»».exe
+del /f /s /q "%cd%\update.bat" 
+
+
